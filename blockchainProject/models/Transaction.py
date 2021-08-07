53732f60a117689e2b32a2818d7ffc4b6707f629
Jacob Tonna <Jtonna.yt@gmail.com>
2021-08-07 15:27:18 -0700

    trashed code temporarily

--- blockchainProject/models/Transaction.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-
-class Transaction:
-    def __init__(self, subscriber_count):
-        self.timestamp = datetime.datetime.now()
-        self.subscriber_count = subscriber_count
--- blockchainProject/models/TransactionList.py
+++
@@ -1,3 +0,0 @@
-class TransactionList:
-    def __init__(self):
-        pass
